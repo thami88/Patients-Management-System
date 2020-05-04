ef6c32d5ba5913355715601bcf05b938d7a9eb42
thami <thami@DESKTOP-LQJ1807>
2020-05-04 12:43:04 +0530

    Implement DB connection method in Patient Class

--- Patients_Management_System/src/com/Patient.java
+++ Patients_Management_System/src/com/Patient.java
@@ -1,5 +1,27 @@
 package com;
 
+import java.sql.*; 
+
 public class Patient {
+	
+	// Method to Connect Database /------------------------------------------------------------
+	
+	private Connection connect() {
+		
+		Connection con = null; 
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con = DriverManager.getConnection("jdbc:mysql://localhost/patient_management", "root", "");
+			
+		} catch (Exception e) {
+			// Handle exception
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	} // End of DB Connection method /---------------------------------------------------------------------
 
 }
